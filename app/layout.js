8048be8d2a291e1a64c003e3b63bea015ffed01b
Ankit Suyal <waitonehour@gmail.com>
2025-04-22 12:13:03 +0530

    Added responsive Navbar with git switch flow

---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "KisanConnect | Bridging Farmers and Buyers",
+  description: "KisanConnect is a platform that connects local farmers directly with buyers. Discover and buy fresh, organic produce straight from the source, empowering communities and eliminating middlemen.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar1st from '@/components/Navbar1st';
+
+const Navbar = () => {
+  return (
+    <>
+    <Navbar1st/>
+    </>
+  );
+};
+
+export default Navbar;
